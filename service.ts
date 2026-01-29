568dffbde572e6d2b19dc51a89c635f94fe973cd
JHAJI01 <kr.gautamjha03@gmail.com>
2026-01-29 13:18:41 +0530

    Updated Code in service.ts

---
+++ service.ts
@@ -0,0 +1,18 @@
+import { ToDoModel } from './todo.schema';
+
+export const createTodo = async (title: string) => {
+  const todo = new ToDoModel({ title });
+  return await todo.save();
+};
+
+export const getAllTodos = async () => {
+  return await ToDoModel.find();
+};
+
+export const updateTodo = async (id: string, title: string) => {
+  return await ToDoModel.findByIdAndUpdate(id, { title }, { new: true });
+};
+
+export const deleteTodo = async (id: string) => {
+  return await ToDoModel.findByIdAndDelete(id);
+};
